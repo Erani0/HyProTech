d9f45ae99acfbc6f0f342e286e1bdf6b81716ad6
Dominik Mraček <98663515+YoofeCZ@users.noreply.github.com>
2026-02-06 10:42:46 +0100

    Delete src/main directory

--- src/main/java/com/shailist/hytale/api/transfer/v1/storage/base/ResourceAmount.java
+++
@@ -1,4 +0,0 @@
-package com.shailist.hytale.api.transfer.v1.storage.base;
-
-public record ResourceAmount<T>(T resource, long amount) {
-}
--- src/main/java/com/shailist/hytale/api/transfer/v1/transaction/TransactionContext.java
+++
@@ -1,7 +0,0 @@
-package com.shailist.hytale.api.transfer.v1.transaction;
-
-public interface TransactionContext {
-    static TransactionContext current() {
-        return Transaction.current();
-    }
-}
